bc86c1a2ba100665dc71153f8ad6833ecc702a12
syd333 <sydmiranda@icloud.com>
2021-10-19 16:34:20 -0400

    set

---
+++ SinglyLinkedList/setSingly.js
@@ -0,0 +1,92 @@
+// ** SET
+// changing the value of a node based on its position in the linked list
+
+// PSEUDOCODE
+// function should accept an index and value
+// use our get function to find specific node
+// if no is not found return false
+// if node is found, set the value of that node to be value passed into function & return true
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop(){
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+    shift(){
+        if(!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if (this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
+    unshift(val){
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this; 
+    }
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while (counter !== index){
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+    set(index, val){
+       let foundNode = this.get(index);
+       if (foundNode){
+            foundNode.val = val;
+            return true;
+       } 
+           return false;
+    }
+}
+
+let list = new SinglyLinkedList()
+list.push("HELLO")
+list.push("GOODBYE")
+list.push("!")
+list.push("<3")
+list.push(":)")
